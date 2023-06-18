ea352a9339c9141670bc76019009672d68b79ffc
sulumor <romuald.patfoort@gmail.com>
2023-06-18 16:29:42 +0200

    MEP dessin pendu

--- script.js
+++ script.js
@@ -13,8 +13,11 @@ const newGame = document.getElementById("newGame");
 const test = document.getElementById("test");
 
 //Erreurs
-const error = document.getElementById("error");
 const fault = document.getElementById("fault");
+
+//Canvas
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
 
 //Variables
 let win = 0;
@@ -46,6 +49,7 @@ class pendu {
 
   search(lettre) {
     lettre = lettre.toLowerCase();
+    if (this.character.includes(lettre)) this.miss();
     this.character.push(lettre);
     let s = this.character.length > 1 ? "s" : "";
     essais.textContent = `lettre${s} essayée${s} : ${this.character.join(" ")}`;
@@ -63,18 +67,29 @@ class pendu {
   win() {
     if (this.word === this.play.join("")) {
       win++;
+      play++;
       alert("Vous avez gagné");
+      submit.setAttribute("disabled", "true");
+      test.setAttribute("disabled", "true");
+      parties.textContent = `${win} ${win > 1 ? "victoires" : "victoire"} 
+      / ${play} ${play > 1 ? "parties" : "partie"}`;
     }
   }
 
   miss() {
     this.fault--;
-    if (this.fault !== 0) {
-      fault.textContent =
-        this.fault === 1 ? "reste 1 essai" : `reste ${this.fault} essais`;
-      return;
+    fault.textContent =
+      this.fault === 1 ? "reste 1 essai" : `reste ${this.fault} essais`;
+    this.draw(this.fault);
+
+    if (this.fault === 0) {
+      play++;
+      alert(`Vous avez perdu. Le mot recherché était ${this.word}`);
+      submit.setAttribute("disabled", "true");
+      test.setAttribute("disabled", "true");
+      parties.textContent = `${win} ${win > 1 ? "victoires" : "victoire"} 
+      / ${play} ${play > 1 ? "parties" : "partie"}`;
     }
-    alert(`Vous avez perdu. Le mot recherché était ${this.word}`);
   }
 
   show() {
@@ -91,14 +106,58 @@ class pendu {
   }
 
   reset() {
-    play++;
     while (reponse.firstChild) {
       reponse.removeChild(reponse.firstChild);
     }
+    ctx.clearRect(0, 0, 400, 200);
     essais.textContent = "";
     fault.textContent = "";
-    parties.textContent = `${win} ${win > 1 ? "victoires" : "victoire"} 
-      / ${play} ${play > 1 ? "parties" : "partie"}`;
+    submit.removeAttribute("disabled");
+    test.removeAttribute("disabled");
+  }
+
+  draw(nb) {
+    switch (nb) {
+      case 9:
+        ctx.fillRect(50, 140, 200, 2);
+        break;
+      case 8:
+        ctx.fillRect(90, 30, 3, 110);
+        break;
+      case 7:
+        ctx.fillRect(90, 30, 100, 2);
+        break;
+      case 6:
+        ctx.fillRect(190, 30, 3, 15);
+        break;
+      case 5:
+        ctx.lineWidth = 2;
+        ctx.beginPath();
+        ctx.arc(192, 55, 10, 0, 2 * Math.PI);
+        ctx.stroke();
+        break;
+      case 4:
+        ctx.fillRect(190, 65, 3, 35);
+        break;
+      case 3:
+        ctx.fillRect(190, 75, 25, 2);
+        break;
+      case 2:
+        ctx.moveTo(192, 100);
+        ctx.lineTo(179, 110);
+        ctx.stroke();
+        break;
+      case 1:
+        ctx.fillRect(165, 75, 25, 2);
+        break;
+      case 0:
+        ctx.moveTo(192, 100);
+        ctx.lineTo(210, 110);
+        ctx.stroke();
+        break;
+      default:
+        break;
+    }
   }
 }
 
