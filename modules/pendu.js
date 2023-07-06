aa77c90496d5b2a2884d724dca4083a540690944
sulumor <romuald.patfoort@gmail.com>
2023-07-06 15:06:55 +0200

    Factorisation

--- modules/pendu.js
+++ modules/pendu.js
@@ -4,13 +4,13 @@ import { s, disableBtns } from "../script.js";
 //Variables
 let win = 0;
 let play = 0;
+let score = 0;
 
 export class pendu {
   constructor(mot) {
     this.word = mot;
-    this.play = new Array(this.size()).fill("_");
     this.fault = 10;
-    this.letters = [];
+    this.letters = "";
     this.canvas = new canvas();
     this.show();
   }
@@ -21,93 +21,89 @@ export class pendu {
 
   search(lettre) {
     lettre = lettre.toLowerCase();
-    this.letters.push(lettre);
+    this.letters += lettre + " ";
     document.getElementById("essais").textContent = `lettre${s(
       this.letters
-    )} essayée${s(this.letters)} : ${this.letters.join(" ")}`;
+    )} essayée${s(this.letters)} : ${this.letters}`;
 
-    switch (lettre) {
-      case "e":
-        lettre = "eéèê";
-        break;
-      case "c":
-        lettre = "cç";
-        break;
-      case "u":
-        lettre = "uû";
-        break;
-      case "i":
-        lettre = "iîï";
-        break;
-      case "o":
-        lettre = "oô";
-        break;
-      default:
-        break;
-    }
+    if (lettre === "e") lettre = "eéèê";
+    if (lettre === "c") lettre = "cç";
+    if (lettre === "u") lettre = "uù";
+    if (lettre === "i") lettre = "iîï";
+    if (lettre === "o") lettre = "oô";
 
     if (!new RegExp(`[${lettre}]`, "g").test(this.word)) {
       this.miss();
-      return;
+      return this.winOrLose();
     }
 
     for (let j = 0; j < lettre.length; j++) {
       for (let i = 0; i < this.size(); i++) {
-        if (this.word[i] === lettre[j]) this.play.splice(i, 1, lettre[j]);
+        if (this.word[i] === lettre[j]) {
+          reponse.childNodes[i].textContent = lettre[j];
+        }
       }
     }
+    this.winOrLose();
+  }
 
+  tryWord(w) {
+    if (this.word !== w) {
+      this.miss(3);
+      return this.winOrLose();
+    }
     this.show();
+    this.win();
   }
 
   win() {
-    if (this.word === this.play.join("")) {
-      win++;
-      this.addPlay();
-      disableBtns();
-      alert("Vous avez gagné");
-    }
+    win++;
+    this.addPlay();
+    disableBtns();
+    score += this.fault * 10;
+    document.getElementById("score").textContent = `Score : ${score}`;
+    alert(`Vous avez gagné. Votre nouveau score : ${score}`);
   }
 
   lose() {
-    if (this.fault === 0) {
-      this.addPlay();
-      disableBtns();
-      alert(`Vous avez perdu. Le mot recherché était ${this.word}`);
-    }
+    this.addPlay();
+    disableBtns();
+    this.show();
+    alert(`Vous avez perdu. Le mot recherché était ${this.word}`);
   }
 
-  miss() {
-    this.fault--;
-    document.getElementById("fault").textContent = `reste ${
-      this.fault
-    } essai${s(this.fault)}`;
-    document.getElementById("indiceP").textContent = `Il vous reste ${
-      this.fault
-    } essai${s(this.fault)}`;
-    this.canvas.draw(this.fault);
-
-    if (this.word !== this.play.join("")) this.lose();
+  miss(repeat = 1) {
+    let count = 0;
+    while (count < repeat) {
+      this.fault--;
+      document.getElementById("fault").textContent = `reste ${
+        this.fault
+      } essai${s(this.fault)}`;
+      document.getElementById("indiceP").textContent = `Il vous reste ${
+        this.fault
+      } essai${s(this.fault)}`;
+      this.canvas.draw(this.fault);
+      count++;
+    }
   }
 
   show() {
     if (reponse.hasChildNodes()) {
-      while (reponse.firstChild) {
-        reponse.removeChild(reponse.firstChild);
+      for (let i = 0; i < this.size(); i++) {
+        reponse.childNodes[i].textContent = this.word[i];
       }
-    }
-    for (let i = 0; i < this.size(); i++) {
-      let p = document.createElement("p");
-      p.textContent = this.play[i];
-      reponse.append(p);
+      return;
     }
 
-    this.win();
-    this.lose();
+    for (let i = 0; i < this.size(); i++) {
+      let p = document.createElement("p");
+      p.textContent = "_";
+      reponse.append(p);
+    }
   }
 
   reset() {
-    if (this.word !== this.play.join("") && this.fault > 0) this.addPlay();
+    if (!this.verification() && this.fault > 0) this.addPlay();
     this.canvas = new canvas();
   }
 
@@ -121,13 +117,27 @@ export class pendu {
     if (this.fault < 2)
       return (error.textContent = `Vous n'avez pas assez d'essais pour utiliser cette indice`);
     for (let i = 0; i < this.size(); i++) {
-      if (this.play[i] === "_") {
+      if (reponse.childNodes[i].textContent === "_") {
+        this.miss(2);
         this.search(this.word[i]);
-        for (let j = 0; j < 2; j++) {
-          this.miss();
-        }
         return;
       }
+    }
+  }
+
+  verification() {
+    for (let i = 0; i < this.size(); i++) {
+      if (this.word[i] !== reponse.childNodes[i].textContent) return false;
+    }
+    return true;
+  }
+
+  winOrLose() {
+    if (this.verification()) {
+      return this.win();
+    }
+    if (this.fault <= 0) {
+      return this.lose();
     }
   }
 }
--- script.js
+++ script.js
@@ -12,18 +12,22 @@ const newGame = document.getElementById("newGame");
 const addLetter = document.getElementById("addLetter");
 const addCategorie = document.getElementById("addCategorie");
 const indiceBtn = document.getElementById("indiceBtn");
+const testWord = document.getElementById("testWord");
+const cate = document.querySelector(".cate");
 
 //Input
 const test = document.getElementById("test");
 
 //Image
 const img = document.getElementById("img");
+const croix = document.querySelector(".croix");
 
 //Erreurs
 const fault = document.getElementById("fault");
 const error = document.getElementById("error");
 
 //Initialisation
+let toggleWord = false;
 let r = 0;
 let mots = await newWord();
 let partie = mot();
@@ -54,39 +58,41 @@ async function newWord() {
 }
 
 export function s(arg) {
-  if (Array.isArray(arg)) return arg.length > 1 ? "s" : "";
+  if (typeof arg === "string") return arg.length > 2 ? "s" : "";
   return arg > 1 ? "s" : "";
 }
 
 export function disableBtns() {
-  submit.setAttribute("disabled", "true");
-  test.setAttribute("disabled", "true");
-  addLetter.setAttribute("disabled", "true");
-  indiceBtn.setAttribute("disabled", "true");
+  [submit, test, indiceBtn, testWord].forEach((btn) => {
+    btn.setAttribute("disabled", "true");
+  });
+}
+
+function removeAtt([...queries], att) {
+  queries.forEach((query) => {
+    query.removeAttribute(att);
+  });
+}
+
+function removeText([...queries]) {
+  queries.forEach((query) => {
+    query.textContent = "";
+  });
 }
 
 function reboot() {
   while (reponse.firstChild) {
     reponse.removeChild(reponse.firstChild);
   }
-  essais.textContent = "";
-  fault.textContent = "";
-  error.textContent = "";
+  removeText([essais, fault, error]);
+  removeAtt([categorie, cate], "style");
+  removeAtt([submit, test, indiceBtn, testWord], "disabled");
   document.getElementById("indiceP").textContent = `Il vous reste 10 essais`;
-  categorie.removeAttribute("style");
-  document.querySelector(".cate").removeAttribute("style");
-  submit.removeAttribute("disabled");
-  test.removeAttribute("disabled");
-  addLetter.removeAttribute("disabled");
-  indiceBtn.removeAttribute("disabled");
 }
 
 function toogleBtn() {
   if (indices.style.display === "block") {
-    indices.removeAttribute("style");
-    img.removeAttribute("style");
-    indiceBtn.removeAttribute("style");
-    document.querySelector(".croix").removeAttribute("style");
+    removeAtt([indices, img, indiceBtn, croix], "style");
     error.textContent = "";
   } else {
     indices.style.display = "block";
@@ -96,14 +102,36 @@ function toogleBtn() {
   }
 }
 
+function toggleWordBtn() {
+  if (!toggleWord) {
+    testWord.textContent = "Essayer 1 lettre";
+    test.placeholder = "Notifier le mot voulu";
+    test.setAttribute("maxlength", "20");
+    toggleWord = true;
+    test.focus();
+  } else {
+    testWord.textContent = "Essayer un mot";
+    test.placeholder = "Notifier 1 lettre";
+    test.setAttribute("maxlength", "1");
+    toggleWord = false;
+    test.focus();
+  }
+}
+
 submit.addEventListener("click", () => {
   partie.search(test.value);
   test.value = "";
+  if (partie.fault < 2) indiceBtn.setAttribute("disabled", "true");
 });
 
 test.addEventListener("keypress", (e) => {
   if (e.key === "Enter") {
-    partie.search(test.value);
+    if (toggleWord) {
+      partie.tryWord(test.value.toLowerCase());
+      toggleWordBtn();
+    } else {
+      partie.search(test.value);
+    }
     test.value = "";
     if (partie.fault < 2) indiceBtn.setAttribute("disabled", "true");
   }
@@ -132,9 +160,15 @@ addCategorie.addEventListener("click", () => {
   categorie.style.display = "block";
   partie.miss();
   toogleBtn();
-  document.querySelector(".cate").style.display = "none";
+  cate.style.display = "none";
 });
 
 indiceBtn.addEventListener("click", () => {
   toogleBtn();
 });
+
+testWord.addEventListener("click", () => {
+  toggleWordBtn();
+});
+
+//voir lorsqu'il reste que 2 essais et faire indice lettre ok
